4c405b9f8d5d087f5ba5f0b747ebd49e1bcd076b
m3mpm <zgeorgan@cl-b5.nsk.21-school.ru>
2023-11-21 15:32:33 +0700

    init project

---
+++ src/main/java/com/learn/JavaSpringBeanScopeLearnApplication.java
@@ -0,0 +1,13 @@
+package com.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringBeanScopeLearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSpringBeanScopeLearnApplication.class, args);
+    }
+
+}
